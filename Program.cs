309897d5f7573e4dac50713a7e2dd41a22b2332d
Raneemq1 <raneemalqadi32@gmail.com>
2024-03-01 23:08:39 +0200

    Add a Product

--- Program.cs
+++ Program.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace InventoryManagement
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inventory Management");
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Inventory inventory = new Inventory();
+
+            int answer = -1;
+            Console.WriteLine("Choose an Answer\n1-Add a product\n2-View all products" +
+                "\n3-Edit a product\n4-Delete a product\n5-Search for a product\n" +
+                "6-Exit");
+            answer = int.Parse(Console.ReadLine());
+
+            while (answer != 0&&answer!=6)
+            {
+
+                switch (answer)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            string name;
+                            double price;
+                            int quantity;
+                            Console.Write("Product Name=");
+                            name = Console.ReadLine();
+                            Console.Write("\nProduct Price=");
+                            price = double.Parse(Console.ReadLine());
+                            Console.Write("\nProduct Quantity=");
+                            quantity = int.Parse(Console.ReadLine());
+                            Product product = new Product(name, quantity, price);
+                            inventory.AddProduct(product);
+
+
+                            break;
+                        }
+
+                    default: { break; }
+
+
+                }
+                Console.Clear();
+                Console.WriteLine("Choose an Answer\n1-Add a product\n2-View all products" +
+                "3-Edit a product\n4-Delete a product\n5-Search for a product\n" +
+                "6-Exit");
+                answer = int.Parse(Console.ReadLine());
+
+            }
         }
     }
 }
